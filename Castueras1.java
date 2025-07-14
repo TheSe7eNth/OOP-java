39669d6b8f596cdb87642118ca9032f0e2f31379
TheSe7eNth <158582605+TheSe7eNth@users.noreply.github.com>
2025-07-14 12:11:09 +0000

    updated

--- Castueras1.java
+++ Castueras1.java
@@ -1,8 +1,6 @@
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("Try programiz.pro");
+        System.out.println("Hi I am Stephen!");
     }
 }
